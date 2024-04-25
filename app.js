a3538f13bff023fa5b67194ee7dcf9933372b5d7
Snehal Suryavanshi <ssuryavanshi6222@gmail.com>
2024-04-25 15:10:04 +0530

    new commit

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const ejsmate = require("ejs-mate");
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"/public")));
 app.engine("ejs",ejsmate);
+// Middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.get("/",(req,res)=>{
@@ -33,6 +40,39 @@ app.get("/aess",(req,res)=>{
 app.get("/contact",(req,res)=>{
     res.render("contact.ejs");
 })
+
+
+// POST endpoint to handle form submissions
+app.post("/contact", (req, res) => {
+    const { name, email,phone, Message } = req.body;
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.SECRET,
+        }
+    });
+
+        // Email content
+        let mailOptions = {
+            from: req.bodyemail ,
+            to: process.env.EMAIL ,
+            subject: 'New Form Submission',
+            text: `Name: ${name}\nEmail: ${email}\nPhone No:${phone}\nMessage: ${Message}`
+        };
+    
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            // res.send('Email sent successfully');
+            res.render("emailsent.ejs");
+        }
+    });
+});
 
 app.listen(8080,()=>{
     console.log("app is listening at 8080");
