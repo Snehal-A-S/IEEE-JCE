dfb1e0c5622ef952471797c752d51e6cf3ea0997
Snehal Suryavanshi <ssuryavanshi6222@gmail.com>
2024-05-02 12:10:58 +0530

    new commit

--- init/data.js
+++ init/data.js
@@ -26,20 +26,8 @@ const sampleEvents = [
         eventVenue:"JCE Belagavi",
         eventDescription:"The intention of this technical quiz is to evaluate one’s own understanding in the field of AI, 5G and Engineering life. JCE IEEE student branch conducted technical quiz on AI , 5G and Engineering life for all the students across the department on 17th Feb 2024.",
         eventPoster:"Techtriva.png",
-        eventPhotographs:[
-            {
-                photoname : "Dignitaries on the Dias",
-                photoData : "IN1.jpg",
-            },
-            {
-                photoname: "Talk by Mr Gajendra Deshapande",
-                photoData :"IN2.jpg",
-            },
-            {
-                photoname:"Outset of the Event",
-                photoData:"IN3.jpg",
-            }
-        ] 
+        
+        
     },
     {
         eventName:"A talk on “Navigating Success: A Roadmap For Placement Excellence”",
@@ -47,20 +35,7 @@ const sampleEvents = [
         eventVenue:"JCE Belagavi",
         eventDescription:"The aim of the talk was to provide an insight about the learning experiences of the alumni during college period. She addressed the gathering on the different career prospects for the students and acquainted them on the skill sets required from them to get good careers.",
         eventPoster:"placement.png",
-        eventPhotographs:[
-            {
-                photoname : "Dignitaries on the Dias",
-                photoData : "IN1.jpg",
-            },
-            {
-                photoname: "Talk by Mr Gajendra Deshapande",
-                photoData :"IN2.jpg",
-            },
-            {
-                photoname:"Outset of the Event",
-                photoData:"IN3.jpg",
-            }
-        ] 
+        
 },  
 {
     eventName:"4th International Conference of Emerging Technologies INCET – 2023",
@@ -68,20 +43,7 @@ const sampleEvents = [
     eventVenue:"Jain College of Engineering, Belagavi (The Host Institute)",
     eventDescription:"Top themes that emerged when comparing the insights gained at each of the conference: The theme of the conference is “Emerging Technologies”. INCET is the place where researchers and practitioners can bring together different perspectives to address a common goal. INCET-2023 is the premier conference of Asian Pacific exchanging technical research in Emerging in Technology and provides attendees with a unique opportunity to present their research work.",
     eventPoster:"incet2023.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 {
     eventName:"An “INTERACT Cluster” Program",
@@ -89,20 +51,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"INTERACT Cluster is designed to help teachers learn while engaging in community based activities. The program consists of the segments like demo teaching, evaluation, feedback, story writing & narrating, decision making & fun team work activities. This program isTo provide a high quality upskilling environment to educators that will help them stay up-to-date with the latest teaching technologies.To promote self-development and stress relief for educators through a fun & interactive learning process.To encourage teamwork and relationship building among educators, which can add value to the organization as a whole.Invited Resource Person/Guests: 3Thub Team.1.Chetana Sarang, Founder & CEO, Dattamsh Edtech LLP. 2.Prakash Mugali, Founder & CEO, Enerzi Group.",
     eventPoster:"interact_cluster.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 {
     eventName:"A talk on “A Journey of an Indian Business Woman”",
@@ -110,20 +59,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"To encourage girl students to excel in STEM education by providing them several platforms to develop their leadership skills that can be utilized in their future lives. IEEE-WiE SB specially motivates women candidates to explore their talents all around in all the related fields of STEM education. Also to celebrate an International Women's Day to honor every women all around the world who are involved in technical education. Chief Guest Ms. Soniya Netalkar took us to her journey as a business woman and highlighted women in engineering in her talk.",
     eventPoster:"business_women.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+     
 },
 {
     eventName:"Seminar on “Menstrual Health & Hygiene”",
@@ -131,20 +67,7 @@ const sampleEvents = [
     eventVenue:"Jain College of Engineering, Belagavi.",
     eventDescription:"To create awareness and to educate everyone about periods which will help the people around us to be open about it and not to treat it as an embarrassment. There are girls who are still not knowing what type of hygiene that is supposed to be maintained during those days. In general boys have no idea about such thing happening to a female. It's our responsibility to educate every single person which will help us in breaking that barrier.",
     eventPoster:"menstrual.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 
 {
@@ -153,20 +76,7 @@ const sampleEvents = [
     eventVenue:"KSRP Government School, Machhe, Belagavi",
     eventDescription:"To enrich the lives of the students helping them to overcome their basic needs in expressing and exploring their creative side in bringing their imagination into reality. The aim of this visit is to address the reviews/feedbacks of last time visit and their needs in understanding of basics of some subjects like mathematics, Science and English etc. Alongside IEEE students demonstrated few science models depicting the working of Newton's Laws and also making them to perform best out of some materials provided to them to evaluate their innovation and creativity.",
     eventPoster:"school_16.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 
 {
@@ -175,20 +85,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"On account of International Day for the Elimination of Violence against Women to observe discrimination against Women Pakhwada from 25th November to 10th December 2022- Poster Presentation competition depicting harassment and domestic violence against women was organized. The premise of the event was to raise awareness around the world that women are subjected to domestic violence & harassment. ",
     eventPoster:"poster_presentation.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 
 {
@@ -197,20 +94,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"On account of International Day for the Elimination of Violence against Women- A Skit on Women Harassment” was performed by IEEE students. The premise of the act was to raise awareness around the world that women are subjected to domestic violence.",
     eventPoster:"women_harassment.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 {
     eventName:"JCE ECE IEEE “SCHOOL ADOPTION” Event",
@@ -218,20 +102,7 @@ const sampleEvents = [
     eventVenue:"KSRP Government School, Machhe, Belagavi",
     eventDescription:"To enrich the lives of the students helping them to overcome their basic needs in expressing and exploring their creative side in bringing their imagination into reality. The aim of this visit is to address the reviews/feedbacks of last time visit and their needs in understanding of basics of some subjects like mathematics, Science and English etc.  ",
     eventPoster:"school_3.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 {
     eventName:"Workshop on Machine Learning & Deep Learning",
@@ -239,20 +110,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"To make students aware of the recent, fastly growing technology Artificial Intelligence, Machine Learning & Deep Learning concepts, algorithms, libraries, interfacing through the hardware kits like Raspberry Pi.",
     eventPoster:"workshop_ML.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+     
 },
 {
     eventName:"JCE ECE IEEE adapted a school",
@@ -260,20 +118,7 @@ const sampleEvents = [
     eventVenue:"KSRP Government School, Machhe, Belagavi",
     eventDescription:"To enrich the lives of the students helping them to overcome their basic needs in expressing and exploring their creative side in bringing their imagination into reality. ",
     eventPoster:"school.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+     
 },
 {
     eventName:"Legal Awareness Program on the occasion of Women's day.",
@@ -281,20 +126,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"To spread legal awareness about the laws and fundamental rights of the government this event was organized especially for women candidates. ",
     eventPoster:"legal_p.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+    
 },
 {
     eventName:"Cooking without fire competition on the occasion of Women's day. ",
@@ -302,20 +134,7 @@ const sampleEvents = [
     eventVenue:"JCE Belagavi",
     eventDescription:"To encourage students to participate in the non technical events as they participate in technical events held under IEEE SB especially giving the chance to women candidates to explore their talents all around. ",
     eventPoster:"cooking.png",
-    eventPhotographs:[
-        {
-            photoname : "Dignitaries on the Dias",
-            photoData : "IN1.jpg",
-        },
-        {
-            photoname: "Talk by Mr Gajendra Deshapande",
-            photoData :"IN2.jpg",
-        },
-        {
-            photoname:"Outset of the Event",
-            photoData:"IN3.jpg",
-        }
-    ] 
+     
 },
 
 ]
